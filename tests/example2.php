179bb7e43c94b21e44b2d07bd55c12a010716c19
jtrumbull <jtrumbull@pprnoteco.com>
2015-08-04 00:02:27 -0400

    Updated example

--- tests/example2.php
+++ tests/example2.php
@@ -3,13 +3,18 @@ header('Content-type: text/xml');
 include '../xml-parser.class.php';
 use XMLParser\XMLParser;
 
-class myClass {
+class klass {
 
   public $username = 'john_smith@some.domain';
   public $password = 'secret';
-  public $greeting = 'Johnny boy.';
+  public $greeting = 'Johnny boy';
 
 }
-$x = new myClass();
-$xml = XMLParser::encode(new myClass());
+$data = new klass();
+$data->login_attempts = [
+  '2015/01/01 07:21:00',
+  '2015/01/01 07:21:20',
+  '2015/01/01 07:22:15'
+];
+$xml = XMLParser::encode($data);
 echo $xml->asXML();
